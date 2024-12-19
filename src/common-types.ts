dcc61d3c493ee969ba09d78faf773613a72d35ee
HRadry <79349436+HRadry@users.noreply.github.com>
2024-12-19 12:57:02 -0600

    Added types for test (#2)

---
+++ src/common-types.ts
@@ -0,0 +1,18 @@
+export type PrivilegeNames = {
+    READ_DASHBOARD: 'READ_DASHBOARD',
+    READ_REMITANCES: 'READ_REMITANCES',
+    READ_RECIPIENTS: 'READ_RECIPIENTS',
+    READ_GENERAL_REMITTANCES_REPORT: 'READ_GENERAL_REMITTANCES_REPORT',
+    READ_GENERAL_INSTITUTIONS_REPORT: 'READ_GENERAL_INSTITUTIONS_REPORT',
+    READ_CONCILIATION_REMITTANCES_REPORT:
+        'READ_CONCILIATION_REMITTANCES_REPORT',
+};
+
+export type QueryRequestParams = {
+	startDate?: string
+	endDate?: string
+	senderId?: string[]
+	receiverId?: string[]
+	page?: number
+	pageSize?: number
+}
