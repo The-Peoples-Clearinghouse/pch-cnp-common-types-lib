bda57e2c26a8e51fa8293fe034626e6bf0148d0b
HRadry <adriana@lacamara.dev>
2024-12-19 17:34:59 -0600

    Update package.json

--- src/authorization-types.ts
+++ src/authorization-types.ts
@@ -1,4 +1,4 @@
-export type PrivilegeNames = {
+export const PrivilegeNames = {
     READ_DASHBOARD: 'READ_DASHBOARD',
     READ_REMITANCES: 'READ_REMITANCES',
     READ_RECIPIENTS: 'READ_RECIPIENTS',
@@ -6,8 +6,10 @@ export type PrivilegeNames = {
     READ_GENERAL_INSTITUTIONS_REPORT: 'READ_GENERAL_INSTITUTIONS_REPORT',
     READ_CONCILIATION_REMITTANCES_REPORT:
         'READ_CONCILIATION_REMITTANCES_REPORT',
-};
-
+} as const;
+export type PrivilegeNames =
+    (typeof PrivilegeNames)[keyof typeof PrivilegeNames];
+    
 export type QueryRequestParams = {
 	startDate?: string
 	endDate?: string
