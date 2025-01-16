896dbcaefefdac2d6289314958eac3d927b3920f
Jorge Sebastián Miguel <jorge@lacamara.dev>
2025-01-16 13:34:25 -0600

    Add financial entity and fee definition types to api-types

--- src/api-types.ts
+++ src/api-types.ts
@@ -94,4 +94,54 @@ export type RecipientsIds = {
     recipient: string[];
 };
 
+export const FinancialEntityType = {
+    NATIONAL_DFSP: 'NATIONAL_DFSP',
+    CNP: 'CNP',
+    FOREIGNER_DFSP: 'FOREIGNER_DFSP',
+    SWITCH: 'SWITCH',
+} as const;
+export type FinancialEntityType = keyof typeof FinancialEntityType;
+
+export type FinancialEntity = {
+    financialEntityName: string;
+    financialEntityId: string;
+    financialEntityType: FinancialEntityType;
+};
+
+export const FeeType = {
+    SENDER: 'SENDER',
+    RECEIVER: 'RECEIVER',
+    SWITCH: 'SWITCH',
+    CNP: 'CNP',
+} as const;
+export type FeeType = keyof typeof FeeType;
+
+export const TransferType = {
+    REMMITANCE: 'REMMITANCE',
+    DOMESTIC: 'DOMESTIC',
+} as const;
+export type TransferType = keyof typeof TransferType;
+
+export const CalculationType = {
+    SENDER_MINUS_RECEIVER: 'SENDER_MINUS_RECEIVER',
+    FIXED: 'FIXED',
+    TOTAL_TRANSFER_AMOUNT_PERCENTAGE: 'TOTAL_TRANSFER_AMOUNT_PERCENTAGE',
+    SENDER_FEE_PERCENTAGE: 'SENDER_FEE_PERCENTAGE',
+} as const;
+export type CalculationType = keyof typeof CalculationType;
+
+export type FeeDefinition = {
+    id: string;
+    financialEntityId: string;
+    active: boolean;
+    transferType: TransferType;
+    feeType: FeeType;
+    calculationType: CalculationType; // calculated types applies ONLY to 'CNP' or 'SWITCH' FeeType
+    feesCurrency: string | null;
+    fixedAmount: number | null; // needs to be null if calculationType is dynamic
+    senderFeePercentage: number | null; //goes from inclusive 0 to 100 exclusive
+    totalTransferAmountPercentage: number | null; //goes from inclusive 0 to 100 exclusive
+    startDate: number;
+    endDate: number;
+};
 
